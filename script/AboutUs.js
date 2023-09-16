9c6e00b02645733d1c4ef1aaec30ef43a3d8e1bf
Melany Dalama <melinahir.md@gmail.com>
2023-09-16 01:14:46 -0300

    efectos en las tarjetas, creacion de aboutUs script

---
+++ script/AboutUs.js
@@ -0,0 +1,19 @@
+//JS CON JQUERY
+
+$(document).ready(function() {
+    const tarjeta = $(".info");
+
+   
+    tarjeta.hover(
+        function() {
+            // Dentro de esta función, 'this' se refiere al elemento .info-img actual
+            $(this).find("h4").css("font-size", "2.3rem");
+           
+            $(this).find("h4").css("transition", "all 1s");
+        },
+        function() {
+            // Esta función se llama cuando el mouse sale del elemento .info-img
+            $(this).find("h4").css("font-size", "1.9rem"); 
+        }
+    );
+});
