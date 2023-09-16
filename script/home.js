715c82d1a6e3cc64a1d7c8a210592a3a59e26209
Melany Dalama <melinahir.md@gmail.com>
2023-09-16 00:59:23 -0300

    cambios diseño en SobreNosotros.html, agregado de librería JQuery

--- script/home.js
+++ script/home.js
@@ -17,6 +17,26 @@ document.addEventListener('click', (e) => {
     }
 });
 
+//JS CON JQUERY
+
+$(document).ready(function() {
+    const tarjeta = $(".info");
+
+   
+    tarjeta.hover(
+        function() {
+            // Dentro de esta función, 'this' se refiere al elemento .info-img actual
+            $(this).find("h4").css("font-size", "2.3rem");
+            $(this).find("h4").css("width", "200px");
+            $(this).find("h4").css("height", "90px");
+            $(this).find("h4").css("transition", "all 1s");
+        },
+        function() {
+            // Esta función se llama cuando el mouse sale del elemento .info-img
+            $(this).find("h4").css("font-size", "1.9rem"); 
+        }
+    );
+});
 
 
 
