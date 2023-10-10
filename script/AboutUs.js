ac6eb69e324416dba6ecaa1a9182e9d89fed0f80
Juan Pablo Salerno <juanpablo.salerno@hotmail.com>
2023-10-10 15:12:08 -0300

    Modificaciones para enviar

--- script/AboutUs.js
+++ script/AboutUs.js
@@ -1,18 +1,17 @@
 //JS CON JQUERY
 
-$(document).ready(function() {
+$(document).ready(function () {
     const tarjeta = $(".info");
 
-   
     tarjeta.hover(
-        function() {
+        function () {
             // Dentro de esta función, 'this' se refiere al elemento .info-img actual
             $(this).find("h4").css("font-size", "2.1rem");
             $(this).find("h4").css("transition", "all 1s");
         },
-        function() {
+        function () {
             // Esta función se llama cuando el mouse sale del elemento .info-img
-            $(this).find("h4").css("font-size", "1.9rem"); 
+            $(this).find("h4").css("font-size", "1.9rem");
         }
     );
 });
